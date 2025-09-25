6a0297702e0d2ef23028d78d34e8de7a9067e6c7
SaigoNoKami <vitk14@gmail.com>
2025-09-25 12:54:48 +0300

    fix scroll

--- front/src/app/product/page.tsx
+++ front/src/app/product/page.tsx
@@ -9,7 +9,8 @@ export default async function ProductListPage({
 }: {
     searchParams: ProductFilters
 }) {
-    const products = await fetchProducts(searchParams)
+    const param = await searchParams
+    const products = await fetchProducts(param)
 
     return (
         <div className="bg-gray-100 min-h-screen p-8">
@@ -22,7 +23,7 @@ export default async function ProductListPage({
 
                 <ProductGrid
                     initialProducts={products.items}
-                    searchParams={searchParams}
+                    searchParams={param}
                 />
             </div>
         </div>
